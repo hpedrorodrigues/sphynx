4760ad9788074c56403930be59e72ed12990b2de
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2020-12-10 09:09:40 -0300

    Fixing directory name in command "compression extract"
    - Refactoring command "compression extract"
    - Removing library file "fs.sh"
    - Adjusting command "git open" to not use old function "sx::fs::fullpath"

--- cli/compression/core/extract.sh
+++ cli/compression/core/extract.sh
@@ -3,55 +3,52 @@
 function sx::compression::extract() {
   local -r file_path="${1}"
 
-  if [ ! -f "${file_path}" ]; then
-    sx::log::fatal "The file \"${file_path}\" doesn't exist"
+  if ! [ -f "${file_path}" ]; then
+    sx::log::fatal "No such file \"${file_path}\""
   fi
 
-  local -r file_name="$(basename "${file_path}")"
-  local -r folder_name="${file_name%%.*}"
-  local -r full_path="$(sx::fs::fullpath "${file_path}")"
-  local -r did_folder_exist="$([ -d "${folder_name}" ] && echo 'yes')"
+  local -r extraction_data=(
+    '.tar,tar,tar xf'
+    '.tar.bz2,tar,tar xjf'
+    '.tb2,tar,tar xjf'
+    '.tbz,tar,tar xjf'
+    '.tbz2,tar,tar xjf'
+    '.tar.gz,tar,tar xzf'
+    '.tar.Z,tar,tar xzf'
+    '.taz,tar,tar xzf'
+    '.tgz,tar,tar xzf'
+    '.tar.xz,tar,tar Jxvf'
+    '.txz,tar,tar Jxvf'
+    '.zip,unzip,unzip'
+    '.rar,unrar,unrar'
+    '.gz,gunzip,gunzip'
+  )
 
-  if [ "${did_folder_exist}" ]; then
-    read -r -p "${folder_name} already exists, do you want to overwrite it? (y/n) " -n 1
+  for data in "${extraction_data[@]}"; do
+    local extension="$(echo "${data}" | cut -d ',' -f 1)"
 
-    echo
+    if echo "${file_path}" | grep -qE "${extension}\$"; then
+      local -r required_cli="$(echo "${data}" | cut -d ',' -f 2)"
+      sx::require "${required_cli}"
 
-    if [[ $REPLY =~ ^[Nn]$ ]]; then
-      return
+      local -r file_name="$(basename "${file_path}")"
+      local -r directory_name="${file_name//${extension}/}"
+
+      if [ -d "${directory_name}" ]; then
+        sx::log::fatal "The directory \"${directory_name}\" already exists"
+      fi
+
+      local -r command_prefix="$(echo "${data}" | cut -d ',' -f 3)"
+
+      mkdir -p "${directory_name}" \
+        && cd "${directory_name}" \
+        || exit 1
+
+      eval "${command_prefix} ../${file_path}"
+
+      exit "${?}"
     fi
-  fi
+  done
 
-  mkdir -p "${folder_name}" && cd "${folder_name}" || exit
-
-  case "${file_path}" in
-    *.tar)
-      tar xf "${full_path}"
-      ;;
-    *.tar.bz2 | *.tb2 | *.tbz | *.tbz2)
-      tar xjf "${full_path}"
-      ;;
-    *.tar.gz | *.tar.Z | *.taz | *.tgz)
-      tar xzf "${full_path}"
-      ;;
-    *.tar.xz | *.txz)
-      tar Jxvf "${full_path}"
-      ;;
-    *.zip)
-      unzip "${full_path}"
-      ;;
-    *.rar)
-      unrar "${full_path}"
-      ;;
-    *.gz)
-      gunzip "${full_path}"
-      ;;
-    *)
-      local -r file_extension="${file_name##*.}"
-
-      sx::log::err "Unsupported extension (${file_extension}): \"${file_path}\" cannot be extracted"
-
-      cd .. && [ ! "${did_folder_exist}" ] && rm -r "${folder_name}"
-      ;;
-  esac
+  sx::log::fatal "Cannot extract file \"${file_path}\""
 }
--- cli/git/core/open.sh
+++ cli/git/core/open.sh
@@ -6,27 +6,30 @@ function sx::git::open() {
   local -r file_path="${1:-.}"
 
   if [ ! -e "${file_path}" ]; then
-    sx::log::fatal "File or directory \"${file_path}\" does not exist..."
+    sx::log::fatal "No such file \"${file_path}\""
   fi
 
-  local -r git_url="$(sx::git::remote::url \
-    | sed 's/git@/\/\//g' \
-    | sed 's/.git$//' \
-    | sed 's/http://g' \
-    | sed 's/https://g' \
-    | sed 's/ssh://g' \
-    | sed 's/:/\//g')"
+  local -r git_url="$(
+    sx::git::remote::url \
+      | sed 's/git@/\/\//g' \
+      | sed 's/.git$//' \
+      | sed 's/http://g' \
+      | sed 's/https://g' \
+      | sed 's/ssh://g' \
+      | sed 's/:/\//g'
+  )"
   local -r git_branch="$(sx::git::current_branch)"
   local -r base_url="http:${git_url}"
 
   if [ "${file_path}" = '.' ]; then
-    local -r full_url="$([ "${git_branch}" = "master" ] || [ "${git_branch}" = "main" ] \
+    local -r full_url="$([ "${git_branch}" = 'master' ] || [ "${git_branch}" = 'main' ] \
       && echo "${base_url}" \
       || echo "${base_url}/tree/${git_branch}")"
   else
     local -r root="$(git rev-parse --show-toplevel | sed 's/\//\\\//g')"
-    local -r full_file_path="$(sx::fs::fullpath "${file_path}")"
-    local -r relative_file_path="$(echo "${full_file_path}" | sed "s/${root}\///g")"
+    local -r relative_file_path="$(
+      echo "${PWD}/${file_path}" | sed "s/${root}\///g"
+    )"
     local -r full_url="${base_url}/blob/${git_branch}/${relative_file_path}"
   fi
 
