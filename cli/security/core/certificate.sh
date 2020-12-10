daccaf4c6630467b2ae11e5e4bed0b517ea308af
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2020-12-10 11:06:07 -0300

    Adding command "security certificate" (#27)

---
+++ cli/security/core/certificate.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+function sx::security::certificate::get() {
+  sx::require 'openssl'
+
+  local -r host="${1:-}"
+
+  if [ -z "${host}" ]; then
+    sx::log::fatal 'This function needs a host as first argument'
+  fi
+
+  local -r info="$(
+    echo -e 'GET / HTTP/1.0\nEOT' \
+      | openssl s_client -connect "${host}:443" -servername "${host}" 2>&1
+  )"
+
+  if ! [[ ${info} == *'-----BEGIN CERTIFICATE-----'* ]]; then
+    sx::log::fatal 'No certificate found'
+  fi
+
+  echo "${info}" | openssl x509
+}
+
+function sx::security::certificate::sans() {
+  sx::require 'openssl'
+
+  local -r host="${1:-}"
+
+  if [ -z "${host}" ]; then
+    sx::log::fatal 'This function needs a host as first argument'
+  fi
+
+  local -r info="$(
+    echo -e 'GET / HTTP/1.0\nEOT' \
+      | openssl s_client -connect "${host}:443" -servername "${host}" 2>&1
+  )"
+
+  if ! [[ ${info} == *'-----BEGIN CERTIFICATE-----'* ]]; then
+    sx::log::fatal 'No certificate found'
+  fi
+
+  local -r certificate_options="no_aux, no_header, no_issuer, \
+                                no_pubkey, no_serial, no_sigdump, \
+                                no_signame, no_validity, no_version"
+  local -r text=$(
+    echo "${info}" | openssl x509 -text -certopt "${certificate_options}"
+  )
+
+  sx::log::info 'Common Name:\n'
+  echo "${text}" \
+    | grep 'Subject:' \
+    | sed -e 's/^.*CN=//' \
+    | sed -e 's/\/emailAddress=.*//'
+
+  sx::log::info '\nSubject Alternative Name(s):\n'
+  echo "${text}" \
+    | grep -A 1 'Subject Alternative Name:' \
+    | sed -e '2s/DNS://g' -e 's/ //g' \
+    | tr ',' '\n' \
+    | tail -n +2
+}
