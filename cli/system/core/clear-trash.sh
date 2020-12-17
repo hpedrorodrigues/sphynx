01115e311e2cd576ee705535c4f584a91a3463f3
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2020-12-17 17:06:18 -0300

    Checking if sqlite3 is available in command "system clear-trash"

--- cli/system/core/clear-trash.sh
+++ cli/system/core/clear-trash.sh
@@ -4,6 +4,8 @@ function sx::system::clear_trash() {
   sx::system::check_requirements
 
   if sx::os::is_osx; then
+    sx::require 'sqlite3'
+
     sudo rm -rfv /Volumes/*/.Trashes /Volumes/*/.Trash ~/.Trash/* /private/var/log/asl/*.asl
 
     sqlite3 "${HOME}/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV"* 'delete from LSQuarantineEvent'
