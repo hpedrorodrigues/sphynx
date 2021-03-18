39d8cbb434a7381c1356d26c6246635cdcfaef28
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-03-18 10:37:58 -0300

    Adding header to kubernetes commands (logs, exec and tmux)

--- cli/kubernetes/core/logs.sh
+++ cli/kubernetes/core/logs.sh
@@ -14,7 +14,7 @@ function sx::k8s::logs() {
     sx::k8s_command::logs "${namespace}" "${pod}" "${container}" "${previous_log}"
   elif sx::os::is_command_available 'fzf'; then
     local -r options="$(
-      sx::k8s::not_pending_pods "${query}" "${namespace}" "${all_namespaces}"
+      sx::k8s::pod_list "${query}" "${namespace}" "${all_namespaces}" true
     )"
 
     if [ -z "${options}" ]; then
@@ -22,7 +22,7 @@ function sx::k8s::logs() {
     fi
 
     # shellcheck disable=SC2086  # quote this to prevent word splitting
-    local -r selected="$(echo -e "${options}" | fzf ${SX_FZF_ARGS})"
+    local -r selected="$(echo -e "${options}" | fzf --header-lines 1 ${SX_FZF_ARGS})"
 
     if [ -n "${selected}" ]; then
       local -r ns="$(echo "${selected}" | awk '{ print $1 }')"
@@ -36,7 +36,7 @@ function sx::k8s::logs() {
 
     local options
     readarray -t options < <(
-      sx::k8s::not_pending_pods "${query}" "${namespace}" "${all_namespaces}"
+      sx::k8s::pod_list "${query}" "${namespace}" "${all_namespaces}"
     )
 
     if [ "${#options[@]}" -eq 0 ]; then
@@ -73,4 +73,84 @@ function sx::k8s_command::logs() {
     --container "${container}" \
     --follow \
     ${flags}
+}
+
+function sx::k8s::pod_list() {
+  local -r query="${1:-}"
+  local -r namespace="${2:-}"
+  local -r all_namespaces="${3:-false}"
+  local -r print_header="${4:-false}"
+
+  if ${all_namespaces}; then
+    local -r flags='--all-namespaces'
+  elif [ -n "${namespace}" ]; then
+    local -r flags="--namespace ${namespace}"
+  else
+    local -r flags=''
+  fi
+
+  if [ -n "${query}" ]; then
+    local -r selector="${query}"
+  else
+    local -r selector='.*'
+  fi
+
+  # shellcheck disable=SC2016  # Expressions don't expand in single quotes, use double quotes for that
+  local -r template='
+  {{range .items}}
+    {{$namespace := .metadata.namespace}}
+    {{$name := .metadata.name}}
+    {{$createdAt := .metadata.creationTimestamp}}
+    {{range .spec.initContainers}}
+      {{$namespace}}{{","}}{{$name}}{{","}}{{.name}}{{", (Init Container)"}}{{"\n"}}
+    {{end}}
+    {{range .spec.containers}}
+      {{$namespace}}{{","}}{{$name}}{{","}}{{.name}}{{"\n"}}
+    {{end}}
+  {{end}}'
+
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  local -r simple_pods_output="$(
+    sx::k8s::cli get pods \
+      ${flags} \
+      | grep -E "${selector}" 2>/dev/null
+  )"
+
+  local -r header='NAMESPACE,POD NAME,CONTAINER NAME,STATUS,RESTARTS,AGE,DESCRIPTION'
+
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  local -r result="$(
+    sx::k8s::cli get pods \
+      ${flags} \
+      --output go-template \
+      --template="$(sx::k8s::clear_template "${template}")" \
+      | sort -u \
+      | column -t -s ',' \
+      | grep -E "${selector}" 2>/dev/null \
+      | while read -r templated_pod_line; do
+        local pod_name="$(echo "${templated_pod_line}" | awk '{ print $2 }')"
+
+        local simple_pod_line="$(echo -e "${simple_pods_output}" | grep "${pod_name}")"
+        local pod_status="$(echo -e "${simple_pod_line}" | awk '{ print $3 }')"
+
+        if echo "${pod_status}" | grep 'Pending\|ContainerCreating\|Terminating\|CreateContainerConfigError'; then
+          continue
+        fi
+
+        local pod_namespace="$(echo "${templated_pod_line}" | awk '{ print $1 }')"
+        local container_name="$(echo "${templated_pod_line}" | awk '{ print $3 }')"
+        local description="$(echo "${templated_pod_line}" | awk '{ print $4 " " $5 }')"
+
+        local pod_restarts_count="$(echo -e "${simple_pod_line}" | awk '{ print $4 }')"
+        local pod_age="$(echo -e "${simple_pod_line}" | awk '{ print $5 }')"
+
+        echo "${pod_namespace},${pod_name},${container_name},${pod_status},${pod_restarts_count},${pod_age},${description}"
+      done
+  )"
+
+  if ${print_header}; then
+    echo -e "${header}\n${result}" | column -t -s ','
+  else
+    echo -e "${result}" | column -t -s ','
+  fi
 }
