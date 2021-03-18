7430b4ba68d32f761880a5ce141ea63eb236a473
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-03-18 16:39:45 -0300

    Adding global command "version"

---
+++ cli/.internal/global-commands/version.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+readonly brew_directory_prefix='/usr/local/Cellar/sphynx/'
+readonly brew_directory_suffix="/bin/${SPHYNX_EXEC_NAME}"
+
+function sx::self::version() {
+  sx::require_supported_os
+
+  if [[ "${SPHYNX_EXEC}" == *"${brew_directory_prefix}"* ]]; then
+    if sx::os::is_linux; then
+      local -r pm_description='Linuxbrew'
+    else
+      local -r pm_description='Homebrew'
+    fi
+
+    local -r version="$(
+      echo "${SPHYNX_EXEC}" \
+        | sed "s#${brew_directory_prefix}##" \
+        | sed "s#${brew_directory_suffix}##"
+    )"
+
+    echo "${pm_description}: ${version}"
+    return 0
+  fi
+
+  if sx::os::is_command_available 'git'; then
+    local -r remote_url="$(sx::git::remote::url)"
+
+    if [ -n "${remote_url}" ]; then
+      echo "Git (Branch): $(sx::git::current_branch)"
+      return ${?}
+    fi
+  fi
+
+  echo 'dev'
+}
