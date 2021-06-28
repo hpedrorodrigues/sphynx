1b8e287e412f372345ac97d2a4d78d169ce135cc
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-06-27 23:22:45 -0300

    Fixing command clear-trash

--- cli/system/core/clear-trash.sh
+++ cli/system/core/clear-trash.sh
@@ -6,9 +6,27 @@ function sx::system::clear_trash() {
   if sx::os::is_osx; then
     sx::require 'sqlite3'
 
-    sudo rm -rfv /Volumes/*/.Trashes /Volumes/*/.Trash ~/.Trash/* /private/var/log/asl/*.asl
+    for log_file in /private/var/log/asl/*.asl; do
+      if ! [ -f "${log_file}" ]; then
+        break
+      fi
+
+      rm -rf "${log_file}"
+    done
 
     sqlite3 "${HOME}/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV"* 'delete from LSQuarantineEvent'
+
+    osascript -e '
+      tell application "Finder"
+        set trashFiles to items in (path to trash folder)
+        if (count trashFiles) = 0 then
+          return "Trash is empty!"
+        else
+          empty trash
+          return "Done!"
+        end if
+      end tell
+  '
   else
     rm -rf "${HOME}/.local/share/Trash/files/"*
   fi
