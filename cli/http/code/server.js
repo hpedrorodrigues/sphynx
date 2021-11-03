7a8cb7dfdb04e0cf6d702bfed83f0fea873a1826
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-11-02 21:49:02 -0300

    Fixing lint issues

--- cli/http/code/server.js
+++ cli/http/code/server.js
@@ -6,30 +6,33 @@ const port = process.env.PORT || 3000;
 const APPLICATION_JSON = 'application/json';
 
 const listener = async (req, res) => {
-    const buffer = [];
-    for await (const chunk of req) {
-        buffer.push(chunk);
-    }
+  const buffer = [];
+  for await (const chunk of req) {
+    buffer.push(chunk);
+  }
 
-    const data = Buffer.concat(buffer).toString();
-    const body = req.headers['content-type'] !== undefined && req.headers['content-type'].startsWith(APPLICATION_JSON) ?
-        JSON.parse(data) : data;
-    const result = {
-        request: {
-            httpVersion: req.httpVersion,
-            path: req.url,
-            method: req.method,
-            host: req.headers.host,
-            headers: req.headers,
-            body: body,
-        }
-    };
+  const data = Buffer.concat(buffer).toString();
+  const body =
+    req.headers['content-type'] !== undefined &&
+    req.headers['content-type'].startsWith(APPLICATION_JSON)
+      ? JSON.parse(data)
+      : data;
+  const result = {
+    request: {
+      httpVersion: req.httpVersion,
+      path: req.url,
+      method: req.method,
+      host: req.headers.host,
+      headers: req.headers,
+      body: body,
+    },
+  };
 
-    console.info(result, '\n-----');
+  console.info(result, '\n-----');
 
-    res.writeHead(200, { 'Content-Type': APPLICATION_JSON });
-    res.write(JSON.stringify(result));
-    res.end();
+  res.writeHead(200, { 'Content-Type': APPLICATION_JSON });
+  res.write(JSON.stringify(result));
+  res.end();
 };
 
 const server = http.createServer(listener);
