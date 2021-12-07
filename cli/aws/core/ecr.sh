54c8a147c6d3a9516c4aa448c608dcf77797cc85
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-12-07 18:55:03 -0300

    Adding support to delete untagged images from a specific repositories

--- cli/aws/core/ecr.sh
+++ cli/aws/core/ecr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function sx::aws::ecr::delete_untagged() {
+function sx::aws::ecr::clear_untagged() {
   sx::aws::check_requirements
 
   aws ecr describe-repositories \
@@ -23,4 +23,40 @@ function sx::aws::ecr::delete_untagged() {
     done
 
   sx::log::info 'Done!'
+}
+
+function sx::aws::ecr::delete_untagged_from_repository() {
+  sx::aws::check_requirements
+
+  local -r repository_name="${1:-}"
+  if [ -z "${repository_name}" ]; then
+    sx::log::fatal 'This function needs a repository as first argument'
+  fi
+
+  if ! sx::aws::ecr::repository_exists "${repository_name}"; then
+    sx::log::fatal "Repository \"${repository_name}\" not found"
+  fi
+
+  sx::log::info "Deleting untagged images from repository \"${repository_name}\"."
+
+  aws ecr list-images \
+    --repository-name "${repository_name}" \
+    --filter 'tagStatus=UNTAGGED' \
+    --query 'imageIds[*]' \
+    --output 'text' \
+    | while read -r image_id; do
+      aws ecr batch-delete-image \
+        --repository-name "${repository_name}" \
+        --image-ids "imageDigest=${image_id}"
+    done
+
+  sx::log::info 'Done!'
+}
+
+function sx::aws::ecr::repository_exists() {
+  local -r repository_name="${1:-}"
+
+  aws ecr describe-repositories \
+    | jq --exit-status ".repositories[] | select(.repositoryName == \"${repository_name}\") | .repositoryName" \
+      &>/dev/null
 }
