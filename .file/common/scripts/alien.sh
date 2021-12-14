c8c78484b39610abe471f2416fea8c3e3cea0069
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2021-12-14 10:52:08 -0300

    Adding functions to help handle repositories
    - Updating Confluent images to 7.0.1

--- .file/common/scripts/alien.sh
+++ .file/common/scripts/alien.sh
@@ -2,7 +2,7 @@
 
 # External images
 
-export CONFLUENT_VERSION=${CONFLUENT_VERSION:-7.0.0}
+export CONFLUENT_VERSION=${CONFLUENT_VERSION:-7.0.1}
 
 ## Kafka CLI (https://kafka.apache.org)
 ##
---
+++ .file/common/scripts/management.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+## Function to help me handle the Sphynx repository
+##
+## e.g. sphynx open
+## e.g. sphynx status
+function sphynx() {
+  local -r action="${1}"
+  local -r sphynx_repository="${HOME}/Code/Personal/sphynx"
+
+  if [ "${action}" = 'open' ]; then
+    code "${sphynx_repository}"
+  elif [ "${action}" = 'go' ]; then
+    cd "${sphynx_repository}" || return 1
+  elif [ "${action}" = 'status' ]; then
+    (cd "${sphynx_repository}" && git status)
+  else
+    echo "!!! No supported action: \"${action}\"" >&2
+    echo '!!!' >&2
+    echo '!!! Available actions' >&2
+    echo '!!!   - open' >&2
+    echo '!!!   - go' >&2
+    echo '!!!   - status' >&2
+    return 1
+  fi
+}
+
+## Function to help me handle the secrets repository
+##
+## e.g. secrets open
+## e.g. secrets push
+function secrets() {
+  local -r action="${1}"
+  local -r secrets_repository="${HOME}/Code/Personal/secrets"
+
+  if [ "${action}" = 'open' ]; then
+    code "${secrets_repository}"
+  elif [ "${action}" = 'go' ]; then
+    cd "${secrets_repository}" || return 1
+  elif [ "${action}" = 'status' ]; then
+    (cd "${secrets_repository}" && git status)
+  elif [ "${action}" = 'pull' ]; then
+    (
+      cd "${secrets_repository}" \
+        && git add -A \
+        && git stash \
+        && ggpull \
+        && git stash apply
+    )
+  elif [ "${action}" = 'push' ]; then
+    (
+      cd "${secrets_repository}" \
+        && git add -A \
+        && git commit -m '[CLI] Updating Secrets' \
+        && ggpush
+    )
+  else
+    echo "!!! No supported action: \"${action}\"" >&2
+    echo '!!!' >&2
+    echo '!!! Available actions' >&2
+    echo '!!!   - open' >&2
+    echo '!!!   - go' >&2
+    echo '!!!   - status' >&2
+    echo '!!!   - pull' >&2
+    echo '!!!   - push' >&2
+    return 1
+  fi
+}
