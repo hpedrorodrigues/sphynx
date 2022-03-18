ee81d67e735d62c88b6d24640a5a145bfed03827
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2022-03-17 23:49:58 -0300

    Running docopts with the available python version

--- cli/.internal/library/url.sh
+++ cli/.internal/library/url.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 function sx::url::encode() {
-  sx::require 'python'
-
-  python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);" "${@}"
+  sx::python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);" "${@}"
 }
