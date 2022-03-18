49aa97d4cae11cf6944591b5c22bc9522df1807b
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2022-03-17 23:58:06 -0300

    Fixing function `sx::url::encode` for python3

--- cli/.internal/library/url.sh
+++ cli/.internal/library/url.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 function sx::url::encode() {
-  sx::python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);" "${@}"
+  sx::python -c "import sys, urllib.parse; print(urllib.parse.quote_plus(sys.argv[1]));" "${@}"
 }
