cecc70dfd17782a5c8b65e9378adbaae41acbad8
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2022-09-25 20:39:45 -0300

    Adding Vagrant boxes for CentOS and Ubuntu

---
+++ .vms/.scripts/common.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+readonly operating_systems=('centos' 'ubuntu')
+
+function show_operating_systems() {
+  echo "Available OSes:"
+  for operating_system in ${operating_systems[@]}; do
+    echo "- ${operating_system}"
+  done
+}
+
+function check_operating_system() {
+  local -r user_os="${1:-}"
+
+  if [ -z "${user_os}" ]; then
+    echo >&2 "An operating system is required as first argument."
+    echo
+    show_operating_systems
+    exit 1
+  fi
+
+  local is_valid_os=false
+  for operating_system in ${operating_systems[@]}; do
+    if [ "${operating_system}" = "${user_os}" ]; then
+      is_valid_os=true
+      break
+    fi
+  done
+
+  if ! ${is_valid_os}; then
+    echo >&2 "Invalid operating system provided as argument: \"${user_os}\"."
+    echo
+    show_operating_systems
+    exit 1
+  fi
+}
+
---
+++ .vms/.scripts/stop.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+readonly script_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+source "${script_dir}/common.sh"
+
+function main() {
+  local -r user_os="${1:-}"
+
+  check_operating_system "${user_os}"
+
+  cd "${script_dir}/../${user_os}" && vagrant halt
+}
+
+main "${@}"
