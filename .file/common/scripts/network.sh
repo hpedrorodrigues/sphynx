62731c8a75340496845a0bf264d5c81519a1bff5
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-01-27 18:29:36 -0300

    Adding function to generate self-signed certificates

--- .file/common/scripts/network.sh
+++ .file/common/scripts/network.sh
@@ -70,3 +70,44 @@ function promql() {
     "${server}/api/v1/query" \
     | jq
 }
+
+## Generate a self-signed certificate
+##
+## e.g. gen_self_signed_cert example.com
+##
+## References:
+## - https://stackoverflow.com/a/31984753/3691240
+function gen_self_signed_cert() {
+  if ! hash 'openssl' 2>/dev/null; then
+    echo 'The command-line \"openssl\" is not available in your path' >&2
+    return 1
+  fi
+
+  local -r func_name="${FUNCNAME[0]:-${funcstack[1]}}"
+  local -r domain="${1}"
+
+  if [ -z "${domain}" ]; then
+    echo '!!! This function needs a domain as first argument' >&2
+    echo "!!! e.g. ${func_name} example.com" >&2
+    return 1
+  fi
+
+  local -r file_prefix="${domain//./-}"
+
+  local -r keyfile_name="${file_prefix}.key"
+  local -r signing_request_name="${file_prefix}.csr"
+  local -r certificate_name="${file_prefix}.crt"
+
+  if ! [ -s "${keyfile_name}" ]; then
+    openssl genrsa -out "${keyfile_name}" 4096
+    openssl rsa -in "${keyfile_name}" -out "${keyfile_name}"
+  fi
+
+  if ! [ -s "${signing_request_name}" ]; then
+    openssl req -sha256 -new -key "${keyfile_name}" -out "${signing_request_name}" -subj "/CN=${domain}"
+  fi
+
+  if ! [ -s "${certificate_name}" ]; then
+    openssl x509 -req -sha256 -days 365 -in "${signing_request_name}" -signkey "${keyfile_name}" -out "${certificate_name}"
+  fi
+}
