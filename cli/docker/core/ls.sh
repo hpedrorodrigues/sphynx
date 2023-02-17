733a263cb77dfc78991f16f884e12b39aebcbaa9
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-02-16 21:23:44 -0300

    Adding support to show only containers and their ips to `docker ls` (#283)

--- cli/docker/core/ls.sh
+++ cli/docker/core/ls.sh
@@ -3,25 +3,37 @@
 function sx::docker::objects() {
   sx::docker::check_requirements
 
-  sx::log::info 'Running Containers\n'
-  docker container ls \
-    --filter 'status=running' \
-    --filter 'status=restarting' \
-    --filter 'status=removing'
+  local -r show_ips="${1:-}"
 
-  sx::log::info '\nStopped Containers\n'
-  docker container ls \
-    --filter 'status=created' \
-    --filter 'status=paused' \
-    --filter 'status=exited' \
-    --filter 'status=dead'
+  if ${show_ips}; then
+    local -r template='{{.Name}},{{upper .State.Status}},{{range .NetworkSettings.Networks}}{{.IPAddress}} {{end}}'
+    local -r output="$(
+      docker container ls --all --quiet \
+        | xargs docker inspect --format "${template}" \
+        | sed 's#^/##'
+    )"
+    echo -e "NAME,STATUS,IPs\n${output}" | column -t -s ','
+  else
+    sx::log::info 'Running Containers\n'
+    docker container ls \
+      --filter 'status=running' \
+      --filter 'status=restarting' \
+      --filter 'status=removing'
 
-  sx::log::info '\nImages\n'
-  docker images
+    sx::log::info '\nStopped Containers\n'
+    docker container ls \
+      --filter 'status=created' \
+      --filter 'status=paused' \
+      --filter 'status=exited' \
+      --filter 'status=dead'
 
-  sx::log::info '\nNetworks\n'
-  docker network ls
+    sx::log::info '\nImages\n'
+    docker images
 
-  sx::log::info '\nVolumes\n'
-  docker volume ls
+    sx::log::info '\nNetworks\n'
+    docker network ls
+
+    sx::log::info '\nVolumes\n'
+    docker volume ls
+  fi
 }
