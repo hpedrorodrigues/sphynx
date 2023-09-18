9aa532eb6d76fd1e3d95af41cb2497cb6e4bb95a
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-09-18 18:50:57 -0300

    Removing p2i (#319)

--- dotfiles/common/scripts/learning.sh
+++ dotfiles/common/scripts/learning.sh
@@ -12,66 +12,6 @@ function explainshell() {
   sx browser open "https://explainshell.com/explain/${*}?args="
 }
 
-# English learning functions
-
-## Create a PNG image file with the meaning of a single provided word
-##
-## e.g. download_meaning 'steep'
-function download_meaning() {
-  local -r func_name="${FUNCNAME[0]:-${funcstack[1]}}"
-  local -r word="${1}"
-
-  if [ -z "${word}" ]; then
-    echo '!!! This function needs a word as first argument' >&2
-    echo "!!! e.g. ${func_name} struggle" >&2
-    return 1
-  fi
-
-  if ! type 'p2i' 2>/dev/null | grep -q 'is a function\|is a shell function'; then
-    echo 'The command-line \"p2i\" is not available in your path' >&2
-    return 1
-  fi
-
-  p2i \
-    --url "https://www.google.com/search?q=${word}+meaning" \
-    --selector '.lr_container' \
-    --filename "${word}.png"
-}
-
-## The same as {@download_meaning} but it reads the words from the provided file
-##
-## e.g. download_meaning_batch 'words.txt'
-function download_meaning_batch() {
-  local -r func_name="${FUNCNAME[0]:-${funcstack[1]}}"
-  local -r file_path="${1}"
-
-  if [ -z "${file_path}" ]; then
-    echo '!!! This function needs a file path as first argument' >&2
-    echo "!!! e.g. ${func_name} words.txt" >&2
-    return 1
-  fi
-
-  if ! [ -f "${file_path}" ]; then
-    echo "!!! No such file \"${file_path}\"" >&2
-    return 1
-  fi
-
-  if ! [ -s "${file_path}" ]; then
-    echo "!!! Empty file \"${file_path}\"" >&2
-    return 1
-  fi
-
-  local -r file_content="$(tr '[:upper:]' '[:lower:]' <"${file_path}" | sort -u)"
-  echo "${file_content}" | tee "${file_path}"
-  echo -e "\n> $(echo "${file_content}" | wc -l | tr -d ' ') words"
-
-  echo
-
-  while IFS= read -r word; do
-    download_meaning "${word}"
-  done <"${file_path}"
-}
-
 # Translation functions
 
 ## Translate from English to Portuguese (Brazil)
