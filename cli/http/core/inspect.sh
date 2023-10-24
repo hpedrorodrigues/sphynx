d1631a37eae16ebb7f46f056eb6ec98bb5193c98
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-10-23 21:49:37 -0300

    Minor improvements to "http inspect" (#333)
    * Minor improvements to "http inspect"
    
    * fixing lint
    
    * Removing return statement

--- cli/http/core/inspect.sh
+++ cli/http/core/inspect.sh
@@ -5,11 +5,14 @@ function sx::http::inspect() {
   sx::require 'node'
 
   local -r port="${1:-}"
+  local -r status_code="${2:-}"
+  local -r response_body="${3:-}"
 
   sx::log::info 'Now you can make requests in order to inspect their content.'
 
   echo "http://127.0.0.1:${port}" | sx::system::clipboard::copy
   sx::log::info 'Server address copied to clipboard!\n'
 
-  PORT="${port}" exec node "${SPHYNX_NAMESPACE_DIR}/code/server.js"
+  PORT="${port}" STATUS_CODE="${status_code}" RESPONSE_BODY="${response_body}" \
+    exec node "${SPHYNX_NAMESPACE_DIR}/code/server.js"
 }
