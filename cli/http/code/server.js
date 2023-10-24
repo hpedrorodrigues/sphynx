142755d321ae664f748ec63d4cae06277cfed069
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-10-23 21:43:33 -0300

    Minor improvements to "http inspect"

--- cli/http/code/server.js
+++ cli/http/code/server.js
@@ -1,45 +1,63 @@
 const http = require('http');
+const util = require('util');
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
+const statusCode = process.env.STATUS_CODE || 200;
+const responseBody = process.env.RESPONSE_BODY;
 
 const APPLICATION_JSON = 'application/json';
 
+const decodeUrl = (url) => {
+  return { raw: url, content: decodeURIComponent(url) };
+};
+
 const deserializeBody = (contentType, data) => {
+  if (data === '') {
+    return '<No body>';
+  }
+
   if (contentType === undefined || !contentType.startsWith(APPLICATION_JSON)) {
     return data;
   }
 
   try {
-    return JSON.parse(data);
+    return { raw: data, content: JSON.parse(data) };
   } catch (e) {
     return data;
   }
 };
 
 const listener = async (req, res) => {
+  console.info('-----\n');
+
   const buffer = [];
   for await (const chunk of req) {
     buffer.push(chunk);
   }
 
   const data = Buffer.concat(buffer).toString();
-  const body = deserializeBody(req.headers['content-type'], data);
-  const result = {
-    request: {
-      httpVersion: req.httpVersion,
-      path: req.url,
-      method: req.method,
-      host: req.headers.host,
-      headers: req.headers,
-      body: body,
-    },
+  const output = {
+    httpVersion: req.httpVersion,
+    path: decodeUrl(req.url),
+    method: req.method,
+    headers: req.headers,
+    body: deserializeBody(req.headers['content-type'], data),
+    clientAddress: `${req.socket.remoteAddress}:${req.socket.remotePort}`,
   };
 
-  console.info(result, '\n-----');
+  console.info(
+    util.inspect(output, {
+      depth: Infinity,
+      colors: true,
+      compact: false,
+      sorted: true,
+    }),
+    '\n\n'
+  );
 
-  res.writeHead(200, { 'Content-Type': APPLICATION_JSON });
-  res.write(JSON.stringify(result));
+  res.writeHead(statusCode, { 'Content-Type': APPLICATION_JSON });
+  res.write(responseBody || JSON.stringify(output));
   res.end();
 };
 
