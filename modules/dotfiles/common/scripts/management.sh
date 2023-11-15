66ec8f0d8929f71c292471028969aad1529763b0
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-11-15 11:42:38 -0300

    Adding management function for notes (#352)
    * Adding management function for notes
    
    * Bump OpenSSL

--- modules/dotfiles/common/scripts/management.sh
+++ modules/dotfiles/common/scripts/management.sh
@@ -6,19 +6,19 @@
 ## e.g. sphynx status
 function sphynx() {
   local -r action="${1}"
-  local -r sphynx_repository="${HOME}/Code/Personal/sphynx"
+  local -r sphynx_directory="${HOME}/Code/Personal/sphynx"
 
   if [ "${action}" = 'code' ]; then
-    code "${sphynx_repository}"
+    code "${sphynx_directory}"
   elif [ "${action}" = 'go' ]; then
-    cd "${sphynx_repository}" || return 1
+    cd "${sphynx_directory}" || return 1
   elif [ "${action}" = 'status' ]; then
-    (cd "${sphynx_repository}" && git status)
+    (cd "${sphynx_directory}" && git status)
   elif [ "${action}" = 'repo' ]; then
-    (cd "${sphynx_repository}" && sx git open)
+    (cd "${sphynx_directory}" && sx git open)
   elif [ "${action}" = 'run' ]; then
     # shellcheck disable=SC2068  # Double quote array expansions
-    (cd "${sphynx_repository}" && ${@:2})
+    (cd "${sphynx_directory}" && ${@:2})
   else
     if [ -z "${action}" ]; then
       echo "!!! No action provided." >&2
@@ -42,17 +42,17 @@ function sphynx() {
 ## e.g. secrets push
 function secrets() {
   local -r action="${1}"
-  local -r secrets_repository="${HOME}/Code/Personal/secrets"
+  local -r secrets_directory="${HOME}/Code/Personal/secrets"
 
   if [ "${action}" = 'code' ]; then
-    code "${secrets_repository}"
+    code "${secrets_directory}"
   elif [ "${action}" = 'go' ]; then
-    cd "${secrets_repository}" || return 1
+    cd "${secrets_directory}" || return 1
   elif [ "${action}" = 'status' ]; then
-    (cd "${secrets_repository}" && git status)
+    (cd "${secrets_directory}" && git status)
   elif [ "${action}" = 'pull' ]; then
     (
-      cd "${secrets_repository}" \
+      cd "${secrets_directory}" \
         && git add -A \
         && git stash \
         && ggpull \
@@ -60,16 +60,69 @@ function secrets() {
     )
   elif [ "${action}" = 'push' ]; then
     (
-      cd "${secrets_repository}" \
+      cd "${secrets_directory}" \
         && git add -A \
-        && git commit -m '[CLI] Updating Secrets' \
+        && git commit -m '[Auto] Updating Secrets' \
         && ggpush
     )
   elif [ "${action}" = 'repo' ]; then
-    (cd "${secrets_repository}" && sx git open)
+    (cd "${secrets_directory}" && sx git open)
   elif [ "${action}" = 'run' ]; then
     # shellcheck disable=SC2068  # Double quote array expansions
-    (cd "${secrets_repository}" && ${@:2})
+    (cd "${secrets_directory}" && ${@:2})
+  else
+    if [ -z "${action}" ]; then
+      echo "!!! No action provided." >&2
+    else
+      echo "!!! No supported action: \"${action}\"" >&2
+    fi
+    echo '!!!' >&2
+    echo '!!! Available actions' >&2
+    echo '!!!   - code' >&2
+    echo '!!!   - go' >&2
+    echo '!!!   - status' >&2
+    echo '!!!   - pull' >&2
+    echo '!!!   - push' >&2
+    echo '!!!   - repo' >&2
+    echo '!!!   - run' >&2
+    return 1
+  fi
+}
+
+## Function to help me handle personal notes
+##
+## e.g. notes code
+## e.g. notes push
+function notes() {
+  local -r action="${1}"
+  local -r notes_directory="${HOME}/Code/Personal/secrets/modules/notes"
+
+  if [ "${action}" = 'code' ]; then
+    code "${notes_directory}"
+  elif [ "${action}" = 'go' ]; then
+    cd "${notes_directory}" || return 1
+  elif [ "${action}" = 'status' ]; then
+    (cd "${notes_directory}" && git status)
+  elif [ "${action}" = 'pull' ]; then
+    (
+      cd "${notes_directory}" \
+        && git add -A \
+        && git stash \
+        && ggpull \
+        && git stash apply
+    )
+  elif [ "${action}" = 'push' ]; then
+    (
+      cd "${notes_directory}" \
+        && git add . \
+        && git commit -m '[Auto] Updating notes' \
+        && ggpush
+    )
+  elif [ "${action}" = 'repo' ]; then
+    (cd "${notes_directory}" && sx git open)
+  elif [ "${action}" = 'run' ]; then
+    # shellcheck disable=SC2068  # Double quote array expansions
+    (cd "${notes_directory}" && ${@:2})
   else
     if [ -z "${action}" ]; then
       echo "!!! No action provided." >&2
