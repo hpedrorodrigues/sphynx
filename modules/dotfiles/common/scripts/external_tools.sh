7687923c4c0ff92eb689756516e4ba832cf735bd
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2023-11-17 19:07:33 -0300

    Bump docker images (#354)
    * Bump docker images
    
    * Renaming dockerhub repository
    
    * Adding common function for external tools
    
    * Renaming docker repository back to alien
    
    * Bump confluent images

--- modules/dotfiles/common/scripts/external_tools.sh
+++ modules/dotfiles/common/scripts/external_tools.sh
@@ -2,7 +2,7 @@
 
 # External images
 
-export CONFLUENT_VERSION=${CONFLUENT_VERSION:-7.5.0}
+export CONFLUENT_VERSION=${CONFLUENT_VERSION:-7.5.2}
 
 ## Kafka CLI (https://kafka.apache.org)
 ##
@@ -38,18 +38,17 @@ function kcli() {
 
 export DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-hpedrorodrigues/alien}"
 
-## Prettier (https://prettier.io)
+## Base function responsible for running external tools
 ##
-## e.g. prettier --check '*/**/*.{yml,yaml}'
-function prettier() {
-  local -r tool_name="${FUNCNAME[0]:-${funcstack[1]}}"
+## e.g. external_tool <tool-name> <args>...
+function external_tool() {
+  local -r tool_name="${1}"
   local -r image="${DOCKER_REPOSITORY}:${tool_name}"
 
   if { [ -n "${ZSH_VERSION:-}" ] && whence -cp "${tool_name}" &>/dev/null; } \
     || { type -P "${tool_name}" &>/dev/null; }; then
     # shellcheck disable=SC2068  # Double quote array expansions
-    # shellcheck disable=SC2086  # quote this to prevent word splitting
-    command ${tool_name} ${@}
+    command ${tool_name} ${@:2}
     return ${?}
   elif ! hash 'docker' 2>/dev/null; then
     echo 'The command-line \"docker\" is not available in your path' >&2
@@ -63,114 +62,45 @@ function prettier() {
       return 1
     fi
 
+    # shellcheck disable=SC2068  # Double quote array expansions
     docker run \
       --name "${tool_name}" \
       --rm \
       --interactive \
-      --volume "${PRETTIER_DIR:-${PWD}}:/mnt" \
-      "${image}" "${@}"
+      --volume "${PWD}:/mnt" \
+      "${image}" ${@:2}
   fi
+}
+
+## Prettier (https://prettier.io)
+##
+## e.g. prettier --check '*/**/*.{yml,yaml}'
+function prettier() {
+  # shellcheck disable=SC2068  # Double quote array expansions
+  external_tool 'prettier' ${@}
 }
 
 ## Hadolint (https://github.com/hadolint/hadolint)
 ##
 ## e.g. hadolint < <path-to-Dockerfile>
 function hadolint() {
-  local -r tool_name="${FUNCNAME[0]:-${funcstack[1]}}"
-  local -r image="${DOCKER_REPOSITORY}:${tool_name}"
-
-  if { [ -n "${ZSH_VERSION:-}" ] && whence -cp "${tool_name}" &>/dev/null; } \
-    || { type -P "${tool_name}" &>/dev/null; }; then
-    # shellcheck disable=SC2068  # Double quote array expansions
-    # shellcheck disable=SC2086  # quote this to prevent word splitting
-    command ${tool_name} ${@}
-    return ${?}
-  elif ! hash 'docker' 2>/dev/null; then
-    echo 'The command-line \"docker\" is not available in your path' >&2
-    return 1
-  else
-    local -r state="$(
-      docker inspect --format '{{.State.Running}}' "${tool_name}" 2>/dev/null
-    )"
-    if [ "${state}" = 'true' ]; then
-      echo "There's already an instance of \"${tool_name}\" running." >&2
-      return 1
-    fi
-
-    docker run \
-      --name "${tool_name}" \
-      --rm \
-      --interactive \
-      --volume "${HADOLINT_DIR:-${PWD}}:/mnt" \
-      "${image}" "${@}"
-  fi
+  # shellcheck disable=SC2068  # Double quote array expansions
+  external_tool 'hadolint' ${@}
 }
 
 ## ShellCheck (https://github.com/koalaman/shellcheck)
 ##
 ## e.g. shellcheck <script-file>
 function shellcheck() {
-  local -r tool_name="${FUNCNAME[0]:-${funcstack[1]}}"
-  local -r image="${DOCKER_REPOSITORY}:${tool_name}"
-
-  if { [ -n "${ZSH_VERSION:-}" ] && whence -cp "${tool_name}" &>/dev/null; } \
-    || { type -P "${tool_name}" &>/dev/null; }; then
-    # shellcheck disable=SC2068  # Double quote array expansions
-    # shellcheck disable=SC2086  # quote this to prevent word splitting
-    command ${tool_name} ${@}
-    return ${?}
-  elif ! hash 'docker' 2>/dev/null; then
-    echo 'The command-line \"docker\" is not available in your path' >&2
-    return 1
-  else
-    local -r state="$(
-      docker inspect --format '{{.State.Running}}' "${tool_name}" 2>/dev/null
-    )"
-    if [ "${state}" = 'true' ]; then
-      echo "There's already an instance of \"${tool_name}\" running." >&2
-      return 1
-    fi
-
-    docker run \
-      --name "${tool_name}" \
-      --rm \
-      --interactive \
-      --env SHELLCHECK_OPTS \
-      --volume "${SHELLCHECK_DIR:-${PWD}}:/mnt" \
-      "${image}" "${@}"
-  fi
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  # shellcheck disable=SC2068  # Double quote array expansions
+  external_tool 'shellcheck' ${SHELLCHECK_OPTS:-} ${@}
 }
 
 ## shfmt (https://github.com/mvdan/sh)
 ##
 ## e.g. shfmt -l -w <script-file>
 function shfmt() {
-  local -r tool_name="${FUNCNAME[0]:-${funcstack[1]}}"
-  local -r image="${DOCKER_REPOSITORY}:${tool_name}"
-
-  if { [ -n "${ZSH_VERSION:-}" ] && whence -cp "${tool_name}" &>/dev/null; } \
-    || { type -P "${tool_name}" &>/dev/null; }; then
-    # shellcheck disable=SC2068  # Double quote array expansions
-    # shellcheck disable=SC2086  # quote this to prevent word splitting
-    command ${tool_name} -i 2 -ci -bn ${@}
-    return ${?}
-  elif ! hash 'docker' 2>/dev/null; then
-    echo 'The command-line \"docker\" is not available in your path' >&2
-    return 1
-  else
-    local -r state="$(
-      docker inspect --format '{{.State.Running}}' "${tool_name}" 2>/dev/null
-    )"
-    if [ "${state}" = 'true' ]; then
-      echo "There's already an instance of \"${tool_name}\" running." >&2
-      return 1
-    fi
-
-    docker run \
-      --name "${tool_name}" \
-      --rm \
-      --interactive \
-      --volume "${SHFMT_DIR:-${PWD}}:/mnt" \
-      "${image}" -i 2 -ci -bn "${@}"
-  fi
+  # shellcheck disable=SC2068  # Double quote array expansions
+  external_tool 'shfmt' -i 2 -ci -bn ${@}
 }
