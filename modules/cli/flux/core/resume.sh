0c171e33c60b6743c15bb05550b695812a36c9bb
Pedro Rodrigues <10264635+hpedrorodrigues@users.noreply.github.com>
2026-02-20 22:01:06 -0300

    feat: add commands for flux

---
+++ modules/cli/flux/core/resume.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+function sx::flux::resume() {
+  sx::flux::check_requirements
+
+  local -r query="${1:-}"
+  local -r namespace="${2:-}"
+  local -r all_namespaces="${3:-false}"
+
+  if sx::os::is_command_available 'fzf'; then
+    local -r options="$(
+      sx::flux::resources "${query}" "${namespace}" "${all_namespaces}"
+    )"
+
+    if [ -z "${options}" ]; then
+      sx::log::fatal 'No resources found'
+    fi
+
+    # shellcheck disable=SC2086  # quote this to prevent word splitting
+    local -r selected="$(echo -e "${options}" | fzf ${SX_FZF_ARGS})"
+
+    if [ -n "${selected}" ]; then
+      local -r ns="$(echo "${selected}" | awk '{ print $1 }')"
+      local -r kind="$(echo "${selected}" | awk '{ print $2 }')"
+      local -r name="$(echo "${selected}" | awk '{ print $3 }')"
+
+      sx::flux_command::resume "${ns}" "${kind}" "${name}"
+    fi
+  else
+    export PS3=$'\n''Please, choose the resource: '$'\n'
+
+    local options
+    readarray -t options < <(
+      sx::flux::resources "${query}" "${namespace}" "${all_namespaces}"
+    )
+
+    if [ "${#options[@]}" -eq 0 ]; then
+      sx::log::fatal 'No resources found'
+    fi
+
+    select selected in "${options[@]}"; do
+      local -r ns="$(echo "${selected}" | awk '{ print $1 }')"
+      local -r kind="$(echo "${selected}" | awk '{ print $2 }')"
+      local -r name="$(echo "${selected}" | awk '{ print $3 }')"
+
+      sx::flux_command::resume "${ns}" "${kind}" "${name}"
+      break
+    done
+  fi
+}
+
+function sx::flux_command::resume() {
+  local -r ns="${1}"
+  local -r kind="${2}"
+  local -r name="${3}"
+
+  local -r flux_type="$(sx::flux::kind_to_type "${kind}")"
+
+  sx::log::info "Resuming ${kind} \"${ns}/${name}\"\n"
+
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  sx::flux::cli resume ${flux_type} "${name}" --namespace "${ns}"
+}
---
+++ modules/cli/flux/shared/shared.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+export SX_FLUX_DEFAULT_RESOURCES='GitRepository,HelmRepository,OCIRepository,Bucket,HelmChart,Kustomization,HelmRelease,Alert,Provider,Receiver,ImageRepository,ImagePolicy,ImageUpdateAutomation'
+export SX_FLUX_RESOURCES="${SX_FLUX_RESOURCES:-${SX_FLUX_DEFAULT_RESOURCES}}"
+
+export SX_FLUXCTL="${SX_FLUXCTL:-flux}"
+
+function sx::flux::check_requirements() {
+  sx::require_supported_os
+  sx::require 'flux'
+  sx::require 'kubectl'
+}
+
+function sx::flux::cli() {
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  ${SX_FLUXCTL} "${@}"
+}
+
+function sx::flux::resources() {
+  local -r query="${1:-}"
+  local -r namespace="${2:-}"
+  local -r all_namespaces="${3:-false}"
+
+  if ${all_namespaces}; then
+    local -r flags='--all-namespaces'
+  elif [ -n "${namespace}" ]; then
+    local -r flags="--namespace ${namespace}"
+  else
+    local -r flags=''
+  fi
+
+  if [ -n "${query}" ]; then
+    local -r selector="$(echo "${query}" | sx::string::lowercase)"
+  else
+    local -r selector='.*'
+  fi
+
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  kubectl get "${SX_FLUX_RESOURCES}" \
+    ${flags} \
+    --output custom-columns=NAMESPACE:.metadata.namespace,KIND:.kind,NAME:.metadata.name \
+    --no-headers 2>/dev/null \
+    | sx::string::lowercase \
+    | grep -E "${selector}" 2>/dev/null
+}
+
+function sx::flux::kind_to_type() {
+  local -r kind="${1}"
+
+  case "${kind}" in
+    gitrepository) echo 'source git' ;;
+    helmrepository) echo 'source helm' ;;
+    ocirepository) echo 'source oci' ;;
+    bucket) echo 'source bucket' ;;
+    helmchart) echo 'source chart' ;;
+    kustomization) echo 'kustomization' ;;
+    helmrelease) echo 'helmrelease' ;;
+    alert) echo 'alert' ;;
+    provider) echo 'alert-provider' ;;
+    receiver) echo 'receiver' ;;
+    imagerepository) echo 'image repository' ;;
+    imagepolicy) echo 'image policy' ;;
+    imageupdateautomation) echo 'image update' ;;
+    *) sx::log::fatal "Unknown Flux kind: ${kind}" ;;
+  esac
+}
