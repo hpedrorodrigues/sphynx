73be8afb7d8d61728bf9cd5c2863c754bb1ef5ca
Pedro Rodrigues <10264635+hpedrorodrigues@users.noreply.github.com>
2026-02-20 22:55:01 -0300

    fix: update `kubernetes exec` to work with `-A` flag

--- modules/cli/kubernetes/shared/shared.sh
+++ modules/cli/kubernetes/shared/shared.sh
@@ -64,10 +64,9 @@ function sx::k8s::running_pods() {
     {{end}}
   {{end}}'
 
-  # shellcheck disable=SC2086  # quote this to prevent word splitting
   local -r simple_pods_output="$(
     sx::k8s::cli get pods \
-      ${flags} 2>/dev/null \
+      --all-namespaces 2>/dev/null \
       | grep -E "${selector}" 2>/dev/null
   )"
 
@@ -83,20 +82,20 @@ function sx::k8s::running_pods() {
       | column -t -s ',' \
       | grep -E "${selector}" 2>/dev/null \
       | while read -r templated_pod_line; do
+        local pod_namespace="$(echo "${templated_pod_line}" | awk '{ print $1 }')"
         local pod_name="$(echo "${templated_pod_line}" | awk '{ print $2 }')"
 
-        local simple_pod_line="$(echo -e "${simple_pods_output}" | grep "${pod_name}")"
-        local pod_status="$(echo -e "${simple_pod_line}" | awk '{ print $3 }')"
+        local simple_pod_line="$(echo -e "${simple_pods_output}" | grep "^${pod_namespace} " | grep "${pod_name}")"
+        local pod_status="$(echo -e "${simple_pod_line}" | awk '{ print $4 }')"
 
         if echo "${pod_status}" | grep -q -v 'Running'; then
           continue
         fi
 
-        local pod_namespace="$(echo "${templated_pod_line}" | awk '{ print $1 }')"
         local container_name="$(echo "${templated_pod_line}" | awk '{ print $3 }')"
 
-        local pod_restarts_count="$(echo -e "${simple_pod_line}" | awk '{ print $4 }')"
-        local pod_age="$(echo -e "${simple_pod_line}" | awk '{ print $5 }')"
+        local pod_restarts_count="$(echo -e "${simple_pod_line}" | awk '{ print $5 }')"
+        local pod_age="$(echo -e "${simple_pod_line}" | awk '{ print $6 }')"
 
         echo "${pod_namespace},${pod_name},${container_name},${pod_status},${pod_restarts_count},${pod_age}"
       done
