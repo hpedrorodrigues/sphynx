b8bce2a7edd597079f77d8603592eb00954864c9
Pedro Rodrigues <10264635+hpedrorodrigues@users.noreply.github.com>
2026-07-11 12:07:04 -0300

    refactor: move the pull-apk helpers into the android shared library

--- modules/cli/android/shared/shared.sh
+++ modules/cli/android/shared/shared.sh
@@ -141,6 +141,59 @@ function sx::android::select_device() {
     | sx::android::select_one
 }
 
+function sx::android::pull_apk() {
+  sx::android::check_requirements "${2:-}"
+
+  local -r query="${1:-}"
+  local -r serial="$(sx::android::target_device "${2:-}")"
+
+  local -r package="$(sx::android::select_package "${query}" "${serial}")"
+
+  if [ -z "${package}" ]; then
+    return 1
+  fi
+
+  sx::android::pull_apk_by_package "${package}" "${serial}"
+}
+
+function sx::android::pull_apk_by_package() {
+  local -r package="${1}"
+  local -r serial="${2:-}"
+
+  local apk_paths
+  readarray -t apk_paths < <(sx::android::shell "${serial}" pm path "${package}" | sed 's/package://g')
+
+  if [ "${#apk_paths[@]}" -eq 1 ]; then
+    local -r apk_path="${apk_paths[0]}"
+  else
+    for path in "${apk_paths[@]}"; do
+      if echo "${path}" | grep -q 'base.apk'; then
+        local -r apk_path="${path}"
+        break
+      fi
+    done
+
+    if [ -z "${apk_path:-}" ]; then
+      sx::log::fatal 'No pullable apk found'
+    fi
+  fi
+
+  local -r apk_name="$(sx::android::shell "${serial}" basename "${apk_path}")"
+  local -r new_apk_name="$(sx::android::find_apk_name_by_package "${package}")"
+
+  if [ -n "${serial}" ]; then
+    local -r serial_flags="-s ${serial}"
+  else
+    local -r serial_flags=''
+  fi
+
+  # shellcheck disable=SC2086  # quote this to prevent word splitting
+  sx::android::adb ${serial_flags} pull "${apk_path}" '.'
+  mv "${apk_name}" "${new_apk_name}"
+
+  sx::log::info "APK ${new_apk_name} saved"
+}
+
 function sx::android::shell() {
   local -r serial="${1:-}"
   shift
